d4dd13acbf6c6b511b43181b3d28cf0e158eb586
Ngan Pham <40241719+npham25@users.noreply.github.com>
2021-02-07 16:39:24 -0600

    Add files via upload

---
+++ R/prepMNT.R
@@ -0,0 +1,42 @@
+#' @title Preprocess MNTs data
+#'
+#' @description     This package preprocesses the MNTs data by removing MNTs which have few available values and imputing the zero values
+#'
+#' @param x,pctzero,freqCut,uniqueCut
+#'
+#' @return
+#'
+#' @examples prepMNT(data_mnt_1, pctzero=75, freqCut=19, uniqueCut=20)
+#'
+#' @export prepMNT
+require(missForest)
+require(impute)
+require(magrittr)
+require(imputeLCMD)
+require(imputeLCMD)
+require(caret)
+
+prepMNT <- function(x, pctZero=75, freqCut = 19, uniqueCut = 20) {
+  res <- nearZeroVar(x, freqCut = freqCut, uniqueCut = uniqueCut,saveMetrics = TRUE, names = FALSE,
+                     foreach = FALSE, allowParallel = TRUE)
+  #Count number of zeroes and NAs
+  res$pctZero <- as.vector(apply(x, 2, function(x)  100*sum(x == 0, na.rm=T)/length(na.omit(x))))
+  #Exclude column added
+  res$exclude <- res$pctZero == 100 |
+    (res$freqRatio > freqCut & res$percentUnique < uniqueCut & res$pctZero > pctZero)
+
+  #Remove lipids in x
+  keep <- row.names(res)[res$exclude == FALSE]
+  x <- x[, colnames(x) %in% keep]
+  #Replace 0 by ""
+  x[x == 0] <- ""
+  x <- as.matrix(sapply(x, function(x) as.numeric(as.character(x))))
+  return (QRILC_wrapper(x))
+}
+
+QRILC_wrapper <- function(data, ...) {
+  result <- data %>% log %>% impute.QRILC(., ...) %>% extract2(1) %>% exp
+  return(result)
+}
+
+
