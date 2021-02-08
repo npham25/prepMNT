f697f36cfe82debec77703c4095477b00832bf02
Ngan Pham <40241719+npham25@users.noreply.github.com>
2021-02-08 09:39:27 -0600

    Update prepMNT.R

--- R/prepMNT.R
+++ R/prepMNT.R
@@ -1,48 +1,47 @@
-#' @title Preprocess MNTs data
-#'
-#' @description     This package preprocesses the MNTs data by removing MNTs which have few available values and imputing the zero values
-#'
-#' @param x,pctzero,freqCut,uniqueCut
-#'
-#' @return
-#'
-#' @examples prepMNT(data_mnt_1, pctzero=75, freqCut=19, uniqueCut=20)
-#'
-#' @export prepMNT
-inst_pkg <- function(pkg){
-  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
-  if (length(new.pkg)) 
-    install.packages(new.pkg, dependencies = TRUE)
-  sapply(pkg, require, character.only = TRUE)
-}
-
-inst_pkg(c("dplyr","imputeLCMD","caret","magrittr"))
-require(dplyr)
 require(imputeLCMD)
-require(caret)
 require(magrittr)
-    
-prepMNT <- function(x, pctZero=75, freqCut = 19, uniqueCut = 20) {
-  res <- caret::nearZeroVar(x, freqCut = freqCut, uniqueCut = uniqueCut,saveMetrics = TRUE, names = FALSE,
-                     foreach = FALSE, allowParallel = TRUE)
-  #Count number of zeroes and NAs
-  res$pctZero <- as.vector(apply(x, 2, function(x)  100*sum(x == 0, na.rm=T)/length(na.omit(x))))
-  #Exclude column added
-  res$exclude <- res$pctZero == 100 |
-    (res$freqRatio > freqCut & res$percentUnique < uniqueCut & res$pctZero > pctZero)
 
+prepMNT <- function(x, pctZeroCut=75, freqCut = 19, uniqueCut = 20) 
+  {
+  if (is.null(dim(x))) 
+    x <- matrix(x, ncol = 1)
+  
+  x <- as.matrix(apply(x,2,function(x) as.numeric(as.character(x))))
+  #the ratio of the most common value to the second most common value
+  freqRatio <- apply(x, 2, function(data) {
+    t <- table(data[!is.na(data)])
+    if (length(t) <= 1) {
+      return(0)
+    }
+    w <- which.max(t)
+    return(max(t, na.rm = TRUE)/max(t[-w], na.rm = TRUE))
+  })
+  #	the percentage of distinct values out of the number of total samples
+  lunique <- apply(x, 2, function(data) length(unique(data[!is.na(data)])))
+  percentUnique <- 100 * lunique/apply(x, 2, length)
+  #Percentage of zeros
+  pctZero <- as.vector(apply(x, 2, function(x)  100*sum(x == 0, na.rm=T)/length(na.omit(x))))
+  #Exclude MNTs which have 100% zeros or > 75%
+  rmv <- pctZero == 100 | 
+    (freqRatio > freqCut & percentUnique < uniqueCut & pctZero > pctZeroCut)
   #Remove lipids in x
-  keep <- row.names(res)[res$exclude == FALSE]
-  x <- x[, colnames(x) %in% keep]
+  x <- x[, !rmv]
   #Replace 0 by ""
   x[x == 0] <- ""
-  x <- as.matrix(sapply(x, function(x) as.numeric(as.character(x))))
+  x <- as.matrix(apply(x,2,function(x) as.numeric(as.character(x))))
   return (QRILC_wrapper(x))
-}
+  }
 
 QRILC_wrapper <- function(data, ...) {
-  result <- data %>% log %>% impute.QRILC(., ...) %>% extract2(1) %>% exp
+  result <- data %>% log %>% imputeLCMD::impute.QRILC(., ...) %>% magrittr::extract2(1) %>% exp
   return(result)
 }
 
-
+`%>%` <- function(lhs, rhs) {
+  lhs <- substitute(lhs)
+  rhs <- substitute(rhs)
+  kind <- 1L
+  env <- parent.frame()
+  lazy <- TRUE
+  .External2(magrittr_pipe)
+}
