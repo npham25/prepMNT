8a509315c3067eaf70e54b64c35712d09b5d6788
Ngan Pham <40241719+npham25@users.noreply.github.com>
2021-02-07 19:43:58 -0600

    Update prepMNT.R

--- R/prepMNT.R
+++ R/prepMNT.R
@@ -9,13 +9,18 @@
 #' @examples prepMNT(data_mnt_1, pctzero=75, freqCut=19, uniqueCut=20)
 #'
 #' @export prepMNT
-require(missForest)
-require(impute)
-require(magrittr)
-require(imputeLCMD)
+inst_pkg <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+
+inst_pkg(c("dplyr","imputeLCMD","caret"))
+require(dplyr)
 require(imputeLCMD)
 require(caret)
-
+    
 prepMNT <- function(x, pctZero=75, freqCut = 19, uniqueCut = 20) {
   res <- caret::nearZeroVar(x, freqCut = freqCut, uniqueCut = uniqueCut,saveMetrics = TRUE, names = FALSE,
                      foreach = FALSE, allowParallel = TRUE)
